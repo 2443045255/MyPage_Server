f3a55331f8ee3cc7e247e8b57627364325d90e99
2443045255 <93034610+2443045255@users.noreply.github.com>
2025-03-24 00:16:23 +0800

    2025/03/24 周一 0:16:23.73

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 const http = require('http')
 const path = require('path')
 const express = require('express')
+const socket_io = require("socket.io");
+
 const httpPort = 802
 const app = express()
 
 app.use(express.static(path.join(__dirname, './public')))
-
 const httpServer = http.createServer(app)
 
+const io = socket_io(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["socket_id"],
+    credentials: true
+  }
+})
+
+io.socketsJoin('room_1')
+io.on("connection", (socket) => {
+  console.log(socket.id)
+
+  socket.join('public_1_room') //将用户加入房间
+
+  console.log(socket.rooms);
+
+  console.log(io.of("/").sockets.size);
+
+
+  socket.on("messages", (data) => {
+    socket.emit(data)
+  })
+});
+
+
+
+
 httpServer.listen(httpPort, () => {
-    console.log(`http://127.0.0.1:${httpPort}`);
+  console.log(`http://127.0.0.1:${httpPort}`);
 })
