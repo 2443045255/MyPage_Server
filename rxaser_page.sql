a889703d7461a362bf2e3606b1877ea392d034cd
2443045255 <93034610+2443045255@users.noreply.github.com>
2025-05-09 22:32:35 +0800

    2025/05/09 周五22:32:35.22

--- rxaser_page.sql
+++ rxaser_page.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2025-05-06 20:25:11
+-- Generation Time: 2025-05-09 16:31:56
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -51,26 +51,46 @@ CREATE TABLE IF NOT EXISTS `room_1` (
   `userName` varchar(32) COLLATE utf8mb4_bin NOT NULL,
   `userMsg_Time` longtext COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`Index`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=34 ;
 
 --
 -- 转存表中的数据 `room_1`
 --
 
 INSERT INTO `room_1` (`Index`, `userID`, `userName`, `userMsg_Time`) VALUES
-(1, '111', '111', '["111_1", [2024, 10, 1, 8, 1]]'),
-(2, '222', '222', '["222_1", [2024, 10, 2, 8, 2]]'),
-(3, '222', '222', '["222_2", [2024, 10, 2, 8, 3]]'),
-(4, '333', '333', '["333_1", [2024, 10, 2, 8, 4]]'),
-(5, '444', '444', '["444_1", [2024, 10, 2, 8, 5]]'),
-(6, '444', '444', '["444_2", [2024, 10, 2, 8, 6]]'),
-(7, '555', '555', '["555_1", [2024, 10, 2, 8, 7]]'),
-(8, '666', '666', '["666_1", [2024, 10, 2, 8, 1]]'),
-(9, '666', '666', '["666_2", [2024, 10, 2, 8, 2]]'),
-(10, '666', '666', '["666_3", [2024, 10, 2, 8, 3]]'),
-(11, '666', '666', '["666_4", [2024, 10, 2, 8, 4]]'),
-(12, '666', '666', '["666_5", [2024, 10, 2, 8, 5]]'),
-(13, '666', '666', '["666_6", [2024, 10, 2, 8, 6]]');
+(1, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["123",[2025,5,9,18,8]]'),
+(2, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["1234",[2025,5,9,18,8]]'),
+(3, '5LKI2UPCY9DW9T7I', '5LKI2UPC', '["123",[2025,5,9,18,12]]'),
+(4, '5LKI2UPCY9DW9T7I', '5LKI2UPC', '["1234",[2025,5,9,18,12]]'),
+(5, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["1",[2025,5,9,19,7]]'),
+(6, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["2",[2025,5,9,19,7]]'),
+(7, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["3",[2025,5,9,19,7]]'),
+(8, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["4",[2025,5,9,19,7]]'),
+(9, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["5",[2025,5,9,19,7]]'),
+(10, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["6",[2025,5,9,19,7]]'),
+(11, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["7",[2025,5,9,19,7]]'),
+(12, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["8",[2025,5,9,19,7]]'),
+(13, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["9",[2025,5,9,19,7]]'),
+(14, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["10",[2025,5,9,19,7]]'),
+(15, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["11",[2025,5,9,19,7]]'),
+(16, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["12",[2025,5,9,19,7]]'),
+(17, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["13",[2025,5,9,19,8]]'),
+(18, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["14",[2025,5,9,19,8]]'),
+(19, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["15",[2025,5,9,19,8]]'),
+(20, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["16",[2025,5,9,19,9]]'),
+(21, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["17",[2025,5,9,19,9]]'),
+(22, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["18",[2025,5,9,19,11]]'),
+(23, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["19",[2025,5,9,19,11]]'),
+(24, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["20",[2025,5,9,19,12]]'),
+(25, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["21",[2025,5,9,19,12]]'),
+(26, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["22",[2025,5,9,19,12]]'),
+(27, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["23",[2025,5,9,19,13]]'),
+(28, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["24\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n24",[2025,5,9,19,13]]'),
+(29, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["25",[2025,5,9,19,13]]'),
+(30, '5LKI2UPCY9DW9T7I', '5LKI2UPC', '["26",[2025,5,9,19,16]]'),
+(31, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["123\\n123",[2025,5,9,19,27]]'),
+(32, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["1",[2025,5,9,21,26]]'),
+(33, '4EQ74S6POZO6Y5B1', '4EQ74S6P', '["123",[2025,5,9,22,25]]');
 
 -- --------------------------------------------------------
 
@@ -79,10 +99,19 @@ INSERT INTO `room_1` (`Index`, `userID`, `userName`, `userMsg_Time`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `username` varchar(32) COLLATE utf8mb4_bin NOT NULL,
-  `password` varchar(32) COLLATE utf8mb4_bin NOT NULL,
-  PRIMARY KEY (`username`)
+  `UserID` varchar(32) COLLATE utf8mb4_bin NOT NULL,
+  `UserName` varchar(32) COLLATE utf8mb4_bin NOT NULL,
+  `UserHandPhoto` text COLLATE utf8mb4_bin,
+  PRIMARY KEY (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`UserID`, `UserName`, `UserHandPhoto`) VALUES
+('4EQ74S6POZO6Y5B1', '4EQ74S6P_1234', 'assets/userHandPhoto/YuanShen_4.png'),
+('5LKI2UPCY9DW9T7I', '5LKI2UPC_1223', 'assets/userHandPhoto/YuanShen_7.png');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
